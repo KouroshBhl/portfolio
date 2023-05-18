86237299f87799eb73f66281ffb0729975b248bf
Kourosh Baharloo <kourosh9bhl@gmail.com>
2023-05-18 05:11:50 +0330

    almost finished menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logo from '../assets/Logo.png';
+import { TbMessage2 } from 'react-icons/tb';
 
 const Menu = () => {
   return (
@@ -24,18 +25,62 @@ const Menu = () => {
         </li>
       </ul>
 
-      <div>
-        <button>Hire me</button>
+      <div className='hire__container'>
+        <a href='#' className='hire__btn'>
+          Hire me
+        </a>
+        <TbMessage2 className='hire__icon' />
       </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.header`
-  background-color: #f0f0f0;
-  height: 10rem;
   display: flex;
+  height: 10rem;
+  background-color: #f0f0f0;
   font-size: 1.4rem;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 11.6rem;
+  text-transform: uppercase;
+
+  ul {
+    display: flex;
+  }
+
+  ul li {
+    padding: 4rem;
+  }
+
+  ul li a {
+    letter-spacing: 0.15rem;
+    color: var(--main-font-color-dark);
+  }
+
+  .hire__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 7px;
+    width: 12.4rem;
+    height: 4rem;
+    background-color: var(--main-theme-color);
+    padding: 6px;
+    border-radius: 50px;
+  }
+
+  .hire__btn {
+    border: none;
+    text-transform: uppercase;
+    font-size: inherit;
+    color: var(--main-font-color-light);
+  }
+  .hire__icon {
+    color: var(--main-font-color-light);
+    width: 1.6rem;
+    height: 1.6rem;
+  }
 `;
 
 export default Menu;
